a1811924311f01f64a4e1bbfe955bd6c0d904071
Tejaswi Allam <156018596+Tejaswireddyallam@users.noreply.github.com>
2024-03-14 16:27:44 -0500

    Create computation.js

---
+++ routes/computation.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var random;
+router.get('/', function(req, res, next) {
+  random = Math.random() * 200;
+  var x;
+  if (req.query.x) {
+    x = parseFloat(req.query.x);
+  } else {
+    x = randomValue;
+  }
+  var ans1 = Math.atan(x); 
+  var ans2 = Math.exp(x); 
+  var ans3 = Math.expm1(x);
+
+  var ansstr1 = `Math.atan applied to ${x} is ${ans1}`;
+  var ansstr2 = `Math.exp applied to ${x} is ${ans2}`;
+  var ansstr3 = `Math.expm1 applied to ${x} is ${ans3}`;
+
+  res.send(`${ansstr1}\n${ansstr2}\n${ansstr3}`);
+});
+
+module.exports = router;
